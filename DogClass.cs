d1290e00c41f9e66b5565e1f484edeb72aca04d2
not4fn <klemyatenkoa@mail.ru>
2021-03-15 10:57:59 +0300

    ffisf

---
+++ DogClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPuJAP_15._03
+{
+    class DogClass
+    {
+        public string Name;
+        public string Breed;
+        public float Age;
+        public DogClass()
+        {
+
+        }
+        public DogClass(string name, string breed, float age)
+        {
+            Name = name;
+            Breed = breed;
+            Age = age;
+        }
+        public void setName(string name)
+        {
+            Name = name;
+        }
+        public string getName()
+        {
+            return Name;
+        }
+        public void setBreed(string breed)
+        {
+
+            Breed = breed;
+        }
+        public string getBreed()
+        {
+            return Breed;
+        }
+        public void setAge(string age)
+        {
+            Age = Convert.ToInt64(age);
+        }
+        public float getAge()
+        {
+            return Age;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPuJAP_15._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            DogClass dog= new DogClass();
+            dog.ToString();
+            Console.WriteLine("Введите кличку: ");
+            dog.setName(Console.ReadLine());
+            Console.WriteLine("Введите породу: ");
+            dog.setBreed(Console.ReadLine());
+            Console.WriteLine("Введите возраст: ");
+            dog.setAge(Console.ReadLine());
+            dog.ToString();
+            Console.Clear();
+            Console.ReadKey();
+        }
+    }
+}
